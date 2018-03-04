b0630d94a95d751997ccd67a74da3e5f24af31f3
luiz_mn <luizmontenegroneto@gmail.com>
2018-03-04 01:00:21 -0400

    project sent
    project sent to Udacity 03/04

---
+++ video_vault_ok/CONTENTS.md
@@ -0,0 +1,38 @@
+#Movie Vault
+##Contents
+
+  
+###Files
+The project includes the following files:  
+
+* CONTENTS.md  
+This file.
+
+* `video.py`  
+Formerly called media.py. Defines the class used for the films.
+
+* `collection_listing.py`  
+Formerly called "fresh_tomatoes.py". Used to render the content to display the page in the browser.
+
+* `video_collection.py`  
+File used to store the movies (old entertainment_center.py).
+* README.md  
+Instructions to use the movie site.
+
+
+  
+###Resources used  
+* [W3Schools.com] (https://www.w3schools.com/bootstrap) Tutorials  
+* [The Hitchhiker’s Guide to Python!] (http://docs.python-guide.org/en/latest/)  
+* [Markdown Cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)  
+* [Google Python Style Guide](https://google.github.io/styleguide/pyguide.html)
+* [Udacity README course] (https://www.udacity.com/course/writing-readmes--ud777)
+* [The Movie Database (TMDb) API](https://www.themoviedb.org/documentation/api)  
+* [Discover API Examples] (https://www.themoviedb.org/documentation/api/discover)
+* [Postman Software] (https://www.getpostman.com/)  
+* [Your own movie database in 5 minutes with IMDb API and Perl] (https://bobbelderbos.com/2011/11/movie-database-imdb-api-perl/)  
+* [IMDb Charts - Top Rated Movies] (https://www.imdb.com/chart/top)
+* [JSON encoder and decoder] (https://docs.python.org/2/library/json.html)
+* [Parsing JSON] (http://docs.python-guide.org/en/latest/scenarios/json/)
+* [How to use JSON with Python] (http://developer.rhino3d.com/guides/rhinopython/python-xml-json/)
+* [Stack Overflow] (https://stackoverflow.com)
---
+++ video_vault_ok/video_collection.py
@@ -0,0 +1,73 @@
+# Here you can store films information
+
+import video 
+import collection_listing
+import requests, json
+
+inception = video.Film("Inception",
+                          "A thief, who steals corporate secrets through"
+                          " the use of dream-sharing technology, is given"
+                          " the inverse task of planting an idea into the"
+                          " mind of a CEO.",
+                          "https://www.warnerbros.com/sites/default/files/styles/key_art_270x400/public/inception_keyart.jpg?itok=7jXiglyb", # NOQA 
+                          "https://www.youtube.com/watch?v=d3A3-zSOBT4",
+                          "2010")
+
+batman_dark = video.Film("The Dark Knight",
+                          "A gang of criminals rob a Gotham City mob bank," 
+                          " double-crossing and murdering each other"
+                           " until only the mastermind remains: the Joker,"
+                           " who escapes with the money. Batman, District"
+                           " Attorney Harvey Dent and Lieutenant Jim Gordon"
+                           " form an alliance to rid Gotham of organized"
+                           " crime.",
+                          "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg", # NOQA
+                          "https://www.youtube.com/watch?v=_PZpmTj1Q8Q",
+                          "2008")
+
+transformers = video.Film("Transformers",
+                          "An ancient struggle between two Cybertronian"
+                          " races, the heroic Autobots and the evil Decepticons,"
+                          " comes to Earth, with a clue to the ultimate power"
+                          " held by a teenager.",
+                          "https://upload.wikimedia.org/wikipedia/en/6/66/Transformers07.jpg", # NOQA
+                          "https://www.youtube.com/watch?v=jkNmwrTgWdI",
+                          "2007")
+
+matrix_movie = video.Film("The Matrix",
+                          "A computer hacker learns from mysterious rebels"
+                          " about the true nature of his reality and his role in the war"
+                          " against its controllers.",
+                          "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg", # NOQA
+                          "https://www.youtube.com/watch?v=m8e-FF8MsqU",
+                          "1999")
+                          
+heat_movie = video.Film("Heat",
+                          "A group of professional bank robbers start to feel"
+                          " the heat from police when they unknowingly leave"
+                          " a clue at their latest heist, while both sides"
+                          " attempt to find balance between their personal"
+                          " and professional lives.",
+                          "https://images-na.ssl-images-amazon.com/images/M/MV5BNDc0YTQ5NGEtM2NkYS00MWRhLThiNzAtNmY3NWU3YzNkMjIyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,661,1000_AL_.jpg", # NOQA
+                          "https://www.youtube.com/watch?v=0xbBLJ1WGwQ",
+                          "1995")
+
+lotr_movie = video.Film("The Lord of the Rings: The Two Towers",
+                          "While Frodo and Sam edge closer to Mordor with the"
+                          " help of the shifty Gollum, the divided fellowship"
+                          " makes a stand against Sauron's new ally, Saruman,"
+                          " and his hordes of Isengard.",
+                          "https://images-na.ssl-images-amazon.com/images/M/MV5BMDY0NmI4ZjctN2VhZS00YzExLTkyZGItMTJhOTU5NTg4MDU4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg", # NOQA
+                          "https://www.youtube.com/watch?v=2dlRvAjU_RI",
+                          "2002")
+
+films = (inception, batman_dark, transformers, matrix_movie, heat_movie, 
+         lotr_movie)
+
+
+# Get top 15 movies from IMDB via myapifilms API
+response = requests.get("http://api.myapifilms.com/imdb/top?start=1&end=15&token=2bc908e5-4c25-458b-8653-b4748c2b1975&format=json&data=0") # NOQA
+top_content = json.loads(response.content)
+
+collection_listing.open_films_page(films,top_content)
+
